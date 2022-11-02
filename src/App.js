c2a11e7934b14bc51aa87446b9c5b477895fa301
Kartik <kartikm2219@gmail.com>
2022-11-02 19:32:33 +0530

    na

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Album from './pages/Album';
-import './App.css';
+import Album from "./pages/Album";
+import "./App.css";
+import { Layout } from "antd";
+import Spotify from "./images/Spotify.png";
+import { SearchOutlined, DownCircleOutlined } from "@ant-design/icons";
+const { Footer, Sider, Content } = Layout;
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
-  )
+  return (
+    <Layout>
+      <Sider width={300} className="sideBar">
+        <img src={Spotify} alt="spotify-logo" className="logo" />
+        <div className="searchBar">
+          <span>Search</span>
+          <SearchOutlined style={{ fontSize: "30px" }} />
+        </div>
+      </Sider>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/album" element={<Album />} />
+        </Routes>
+      </Content>
+    </Layout>
+  );
 };
 
 export default App;
