7cadb08e781c12b5624117ec1866d23ac9d202a1
kartikmishraa <kartikm2219@gmail.com>
2022-11-08 03:34:31 +0530

    player hook

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -14,18 +14,50 @@ import { useIPFS } from "../hooks/useIPFS";
 function AudioPlayer({ nftAlbum }) {
   const { resolveLink } = useIPFS();
 
+  const toggle = () => {
+    console.log("toggle");
+  };
+  const toPrevTrack = () => {
+    console.log("toPrevTrack");
+  };
+  const toNextTrack = () => {
+    console.log("toNextTrack");
+  };
   return (
-    <div className="buttons">
-      <img
-        className="cover"
-        src={resolveLink(JSON.parse(nftAlbum[0].metadata).image)}
-        alt="currentCover"
-      />
-      <div>
-        <div className="songTitle">{JSON.parse(nftAlbum[0].metadata).name}</div>
-        <div className="songAlbum">{nftAlbum[0].name}</div>
+    <>
+      <div
+        className="buttons"
+        style={{ width: "300px", justifyContent: "start" }}
+      >
+        <img
+          className="cover"
+          src={resolveLink(JSON.parse(nftAlbum[0].metadata).image)}
+          alt="currentCover"
+        />
+        <div>
+          <div className="songTitle">
+            {JSON.parse(nftAlbum[0].metadata).name}
+          </div>
+          <div className="songAlbum">{nftAlbum[0].name}</div>
+        </div>
       </div>
-    </div>
+      <div>
+        <div className="buttons">
+          <StepBackwardOutlined className="forback" onClick={toPrevTrack} />
+          <PlayCircleFilled className="pauseplay" onClick={toggle} />
+          <StepForwardOutlined className="forback" onClick={toNextTrack} />
+        </div>
+        <div className="buttons">
+          00:00
+          <Slider value={50} className="progress" />
+          00:00
+        </div>
+      </div>
+      <div className="soundDiv">
+        <SoundOutlined />
+        <Slider className="volume" defaultValue={100} tooltipVisible={false} />
+      </div>
+    </>
   );
 }
 
